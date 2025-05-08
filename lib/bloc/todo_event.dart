ea047be636950a1267829f1f4bd10a6030aecaab
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-08 19:18:09 +0700

    add TodoSelectedDate class to handle date selection for todos

--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -16,3 +16,9 @@ final class TodoEventComplete extends TodoEvent {
     required this.index
   });
 }
+final class TodoSelectedDate extends TodoEvent {
+  final DateTime date;
+  TodoSelectedDate({
+    required this.date
+  });
+}
